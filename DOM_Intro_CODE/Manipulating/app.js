7566d642ae7fb9080eb242ed6823759c2af0dece
Brian Bycynski <high5bri@hotmail.com>
2020-10-22 01:47:40 -0400

    Event practice

--- DOM_Intro_CODE/Manipulating/app.js
+++ DOM_Intro_CODE/Manipulating/app.js
@@ -1,12 +1,25 @@
-const allLinks = document.querySelectorAll('a');
+//
+//
+const container = document.querySelector('#container');
 
-// for (let link of allLinks) {
-//     link.innerText = 'I AM A LINK!!!!'
-// }
-
-
-for (let link of allLinks) {
-    link.style.color = 'rgb(0, 108, 134)';
-    link.style.textDecorationColor = 'magenta';
-    link.style.textDecorationStyle = 'wavy'
+for (let i = 0; i < 100; i++) {
+  const newButton = document.createElement('button');
+  newButton.textContent = 'Bozo';
+  container.appendChild(newButton);
 }
+
+// document.body.appendChild(newButton);
+
+// const newImage = document.createElement('img');
+
+// const textInput = document.querySelector('.textInput');
+
+// console.log(newImage);
+// console.log(textInput);
+// newImage.setAttribute(
+//   'src',
+//   'https://images.unsplash.com/photo-1601758260820-6342add57279?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+// );
+// newImage.classList.add('square');
+// // document.body.appendChild(newImage);
+// textInput.insertAdjacentElement('afterend', newImage);
