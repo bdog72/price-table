1915b4e20c9e5fd800e4b9efed04a57599691572
Brian Bycynski <high5bri@hotmail.com>
2020-10-26 01:20:14 -0400

    added new section

---
+++ js/section-2/app1.js
@@ -0,0 +1,90 @@
+//
+//
+
+// const age = 15;
+
+// if (age >= 18) {
+//   console.log(`Sarah can start driving 🚗`);
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`Sarah is too young! She has ${yearsLeft} years`);
+// }
+
+// const birthYear = 2012;
+
+// let century;
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+// console.log(century);
+
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.96;
+
+// const markBMI = parseInt(78 / 1.69 ** 2);
+// const johnBMI = parseInt(92 / 1.96 ** 2);
+
+// console.log(markBMI);
+// console.log(johnBMI);
+
+// const markHigherBMI = markBMI > johnBMI;
+// console.log(markHigherBMI);
+
+// const markString = `Marks BMI (${markBMI}) is higher than Johns BMI (${johnBMI})`;
+
+// const johnString = `Marks BMI (${markBMI}) is lower than Johns BMI (${johnBMI})`;
+
+// if (markBMI > johnBMI) {
+//   console.log(markString);
+// } else {
+//   console.log(johnString);
+// }
+//////////////////////////////////////////
+// const input = parseInt('1991');
+// console.log(input + 18);
+
+// const inputYear = '1972';
+// console.log(Number(inputYear));
+
+// console.log(Number(inputYear) + 18);
+// console.log(Number('bozo'));
+
+// let n = '1' + 1;
+// n = n - 1;
+// console.log(n);
+
+//////////////////////////////////////////
+
+// const money = 110;
+// if (money) {
+//   console.log(`Don't spend it all`);
+// } else {
+//   console.log(`Get a Job Bozo`);
+// }
+
+// let user;
+// console.log(user);
+// if (user === undefined) {
+//   console.log(`User is undefined`);
+// } else {
+//   console.log(`User is defined`);
+// }
+
+//////////////////////////////////////////
+
+const favorite = Number(prompt('Pick a favorite number'));
+
+if (favorite === 5) {
+  console.log(`Cool, ${favorite} is a great number`);
+} else if (favorite === 7) {
+  console.log(`Cool, ${favorite} is also a great number`);
+} else {
+  console.log(`Number is not 5 or 7!`);
+}
+
+
+//////////////////////////////////////////
