26cd430836ab5656a7492d3dd372b462a8a033af
Brian Bycynski <high5bri@hotmail.com>
2020-10-29 01:40:40 -0400

    adding cloud section

--- js/section-3/app1.js
+++ js/section-3/app1.js
@@ -213,12 +213,249 @@
 // const jonas = [firstName, 'Smelt', 2037 - 1991, 'teacher', friends];
 // console.log(jonas);
 
-const calcAge = function (birthYear) {
-  return 2037 - birthYear;
-};
+// const calcAge = function (birthYear) {
+//   return 2037 - birthYear;
+// };
 
-const years = [1990, 1967, 2002, 2010, 2018];
+// const years = [1990, 1967, 2002, 2010, 2018];
 
-const age1 = calcAge(years[1]);
-console.log(age1);
+// const age1 = calcAge(years[1]);
+// console.log(age1);
 /////////////////////////////////////////
+
+// const friends = ['Michael', 'Steven', 'Peter'];
+
+// // Add Elements
+// const newLength = friends.push('Jay');
+// console.log(friends);
+// console.log(newLength);
+
+// friends.unshift('John');
+// console.log(friends);
+
+// // Remove Elements
+// friends.pop(); // Last
+// const popped = friends.pop();
+// console.log(popped);
+// console.log(friends);
+
+// friends.shift(); // First
+// console.log(friends);
+
+// console.log(friends.indexOf('Steven'));
+
+// console.log(friends.includes('Steven'));
+// console.log(friends.includes('Steven1'));
+
+// if (friends.includes('Steven')) {
+//   console.log(`You have a friend named steven`);
+// } else {
+// }
+/////////////////////////////////////////
+
+// const calcTip = function (bill) {
+//   return bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+// };
+
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+// console.log(bills);
+// console.log(tips);
+// console.log(totals);
+
+// const calcTip = billValue => {
+//   const tipPercentage = billValue <= 300 && billValue >= 50 ? '0.15' : '0.20';
+//   const tips = [];
+//   tips.push(tipPercentage * billValue);
+//   console.log(tips);
+//   return `The tip is $${
+//     tipPercentage * billValue
+//   }. Total bill inluding tip is $${tipPercentage * billValue + billValue}`;
+//   // return `The tip is ${tipPercentage}%`;
+// };
+
+// console.log(calcTip(100));
+/////////////////////////////////////////
+
+// const bozo = {
+//   firstName: 'Bozo',
+//   lastName: 'Beak',
+//   age: 48,
+//   job: 'developer',
+//   friends: ['t-bone', 'josey', 'molly'],
+// };
+
+// console.log(
+//   `${bozo.firstName} has ${bozo.friends.length} friends. His best friend is named ${bozo.friends[2]}`
+// );
+
+// console.log(bozo);
+// console.log(bozo.lastName);
+// console.log(bozo['firstName']);
+
+// console.log(bozo);
+
+// const nameKey = 'Name';
+
+// const interestedIn = prompt(
+//   'What do you want to know about Bozo? Choose between firstName, lastName, age, job, friends'
+// );
+
+// if (bozo[interestedIn]) {
+//   console.log(bozo[interestedIn]);
+// } else {
+//   console.log(
+//     `Wrong request! Choose between firstName, lastName, age, job, friends`
+//   );
+// }
+
+// bozo.location = 'Mexico';
+
+// if (interestedIn === 'age' || interestedIn === 'Age') {
+//   alert(bozo.age);
+// }
+
+/////////////////////////////////////////
+
+// const bozo = {
+//   firstName: 'Bozo',
+//   lastName: 'Beak',
+//   birthYear: 1972,
+//   job: 'developer',
+//   friends: ['t-bone', 'josey', 'molly'],
+//   hasDriversLicense: true,
+
+//   calcAge: function () {
+//     return 2020 - this.birthYear;
+//   },
+
+//   getSummary: function () {
+//     return `${this.firstName} is a ${this.calcAge()} year old ${
+//       this.job
+//     }. ${decisions} driver license`;
+//   },
+// };
+
+// console.log(bozo.calcAge());
+
+// const decisions = bozo.hasDriversLicense ? 'He has a' : 'He does not have a';
+
+// console.log(bozo.getSummary());
+
+// console.log(
+//   `${bozo.firstName} is a ${bozo.calcAge()} year old ${
+//     bozo.job
+//   }. ${decisions} driver license`
+// );
+
+/////////////////////////////////////////
+
+// const mM = {
+//   fullName: 'Mark Miller',
+//   mass: 78,
+//   height: 1.69,
+
+//   calcBMI: function () {
+//     this.bmi = this.mass / this.height ** 2;
+//     return this.bmi;
+//   },
+// };
+
+// mM.calcBMI();
+// console.log(mM.bmi);
+
+// const jS = {
+//   fullName: 'John Smith',
+//   mass: 92,
+//   height: 1.95,
+
+//   calcBMI: function () {
+//     this.bmi = this.mass / this.height ** 2;
+//     return this.bmi;
+//   },
+// };
+
+// jS.calcBMI();
+// console.log(jS.bmi);
+
+// const markRoundedBMI = Math.round(mM.bmi);
+// const johnRoundedBMI = Math.round(jS.bmi);
+
+// if (mM.bmi > jS.bmi) {
+//   console.log(
+//     `${mM.fullName}'s (${markRoundedBMI}) BMI is higher than ${jS.fullName}'s (${johnRoundedBMI}) BMI`
+//   );
+// } else {
+//   console.log(
+//     `${jS.fullName}'s (${johnRoundedBMI}) BMI is higher than ${mM.fullName}'s (${markRoundedBMI}) BMI`
+//   );
+// }
+
+/////////////////////////////////////////
+
+// const bozo = [
+//   'Bozo',
+//   'Beak',
+//   2037 - 1991,
+//   'developer',
+//   ['t-bone', 'josey', 'molly'],
+//   true,
+// ];
+
+// const types = [];
+
+// for (let i = 0; i < bozo.length; i++) {
+//   console.log(bozo[i], typeof bozo[i]);
+
+//   // types[i] = typeof bozo[i];
+//   types.push(typeof bozo[i]);
+// }
+
+// console.log(types);
+
+// const years = [1991, 2007, 1969, 2020];
+
+// const ages = [];
+// let i;
+// for (i = 0; i < years.length; i++) {
+//   ages.push(2037 - years[i]);
+// }
+// console.log(ages);
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log('bozo', i);
+// }
+
+// const bozo = [
+//   'Bozo',
+//   'Beak',
+//   2037 - 1991,
+//   'developer',
+//   ['t-bone', 'josey', 'molly'],
+//   true,
+//   false,
+// ];
+
+// for (let i = bozo.length - 1; i >= 0; i--) {
+//   console.log(i, bozo[i]);
+// }
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------Starting Exercise ${exercise}`);
+  for (let reps = 1; reps <= 5; reps++) {
+    console.log(`  -------Reps ${reps}`);
+  }
+}
+
+// const types = [];
+
+// for (let i = 0; i < bozo.length; i++) {
+
+//   console.log(bozo[i], typeof bozo[i]);
+// }
+
+//   // types[i] = typeof bozo[i];
+//   types.push(typeof bozo[i]);
+// }
